15d8bfc1e431756b56ff2b0f7534a512d1c211ec
Bradley Allen <valkyrieuk@Labs-MacBook-Air.local>
2013-12-06 15:18:37 +0000

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChat::Application.config.secret_token = '52d7aeb6ba6864a5292e39eb8f843aab95ea59afb37f2d8be2656c2c313fa245263ac7c91bda99d5486c88acde2924adfd4663230bc948c496923e3fab828dc8'
